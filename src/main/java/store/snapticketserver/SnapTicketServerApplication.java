dabf287007c8babed349d9d570b7fd948a1a18a7
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-06 00:01:36 +0900

    Initial commit

---
+++ src/main/java/store/snapticketserver/SnapTicketServerApplication.java
@@ -0,0 +1,13 @@
+package store.snapticketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapTicketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapTicketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/store/snapticketserver/SnapTicketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package store.snapticketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapTicketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
