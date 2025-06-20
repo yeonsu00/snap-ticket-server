aa84057165dfcfcaacfabd71eea27aeda59ac10b
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-06-21 02:17:30 +0900

    feat: 리프레시 토큰 유효성 검사 request, response 추가

--- src/main/java/store/snapticketserver/user/adapter/in/web/AuthController.java
+++ src/main/java/store/snapticketserver/user/adapter/in/web/AuthController.java
@@ -1,6 +1,9 @@
 package store.snapticketserver.user.adapter.in.web;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import store.snapticketserver.global.response.CommonApiResponse;
 import store.snapticketserver.user.adapter.in.web.request.LogInRequest;
 import store.snapticketserver.user.adapter.in.web.response.LogInResponse;
+import store.snapticketserver.user.adapter.in.web.response.ValidateRefreshTokenResponse;
 
 @Tag(name = "User", description = "User API")
 @RestController
@@ -30,6 +34,29 @@ public class AuthController {
     @PostMapping("/login")
     public CommonApiResponse<LogInResponse> login(@RequestBody LogInRequest logInRequest) {
         return CommonApiResponse.success(new LogInResponse("my-access-token"));
+    }
+
+    @Operation(summary = "리프레시 토큰 유효성 검사 (로그인 필요)", description = "리프레시 토큰 유효성 검사 성공 시 새로운 액세스 토큰 발급합니다.",
+            responses = {
+                    @ApiResponse(
+                            headers = {
+                                    @Header(name = "Set-Cookie", description = "refreshToken", schema = @Schema(type = "string"))
+                            }
+                    )
+            }
+
+    )
+    @Parameters({
+            @Parameter(
+                    name = "Cookie",
+                    description = "refreshToken=my-refresh-token",
+                    in = ParameterIn.HEADER,
+                    required = true
+            )
+    })
+    @PostMapping("/validate")
+    public CommonApiResponse<ValidateRefreshTokenResponse> validateRefreshToken() {
+        return CommonApiResponse.success(new ValidateRefreshTokenResponse("my-new-access-token"));
     }
 
 }
---
+++ src/main/java/store/snapticketserver/user/adapter/in/web/response/ValidateRefreshTokenResponse.java
@@ -0,0 +1,6 @@
+package store.snapticketserver.user.adapter.in.web.response;
+
+public record ValidateRefreshTokenResponse(
+        String nesAccessToken
+) {
+}
